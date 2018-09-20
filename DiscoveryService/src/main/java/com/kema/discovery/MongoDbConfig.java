79a7376aed4e8302e46d73f94c2970430c0db833
kema <xiaofengma2000@gmail.com>
2018-09-20 18:09:10 +1000

    Added Mongo DB Configuration

--- DiscoveryService/src/main/java/com/kema/discovery/MongoDbConfig.java
+++ DiscoveryService/src/main/java/com/kema/discovery/MongoDbConfig.java
@@ -1,14 +1,18 @@
 package com.kema.discovery;
 
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 @Configuration
-public class MongodbConfig {
+public class MongoDbConfig extends AbstractMongoConfiguration{
 
     @Value( "${mongo.host:localhost}" )
     private String host;
@@ -24,9 +28,12 @@ public class MongodbConfig {
         return new MongoClient(host, port);
     }
 
-    @Bean
-    public MongoTemplate mongoTemplate(){
-        return new MongoTemplate(mongoClient(),dbname);
-    }
+//    @Bean
+//    public MongoTemplate mongoTemplate(){
+//        return new MongoTemplate(mongoClient(),dbname);
+//    }
 
+    protected String getDatabaseName() {
+        return dbname;
+    }
 }
--- DiscoveryService/src/main/java/com/kema/discovery/web/EntityObjApi.java
+++ DiscoveryService/src/main/java/com/kema/discovery/web/EntityObjApi.java
@@ -5,14 +5,19 @@ import com.kema.discovery.repository.EntityRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class EntityObjApi {
@@ -41,6 +46,19 @@ public class EntityObjApi {
             return new ResponseEntity<EntityObj>(result, HttpStatus.OK);
         }
         return new ResponseEntity<EntityObj>(HttpStatus.NOT_FOUND);
+    }
+
+    @Autowired
+    ApplicationContext ctx;
+
+    @PostConstruct
+    public void printMongoTemplates(){
+        final Set<Map.Entry<String, MongoTemplate>> entries = ctx.getBeansOfType(MongoTemplate.class).entrySet();
+        for(Map.Entry<String, MongoTemplate> en : entries){
+            System.out.println("Bean name : " + en.getValue() + ", DB name : " + en.getValue().getDb().getName());
+        }
+//        .stream().forEach(en -> {
+//        });
     }
 
 }
